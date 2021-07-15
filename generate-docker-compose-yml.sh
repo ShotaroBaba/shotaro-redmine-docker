6cccd70f3160167d9839b2674ec31ec09e6700f4
ShotaroBaba <bshotaro@gmail.com>
2021-07-15 15:13:19 +0200

    Added web admin client password

--- generate-docker-compose-yml.sh
+++ generate-docker-compose-yml.sh
@@ -3,11 +3,14 @@
 # Set passwords first
 num_pass_char=24
 
+# Generate random password
 redmine_db_password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c $num_pass_char ; echo '')
 redmine_secret_key=$(tr -dc A-Za-z0-9 </dev/urandom | head -c $num_pass_char ; echo '')
 mysql_root_password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c $num_pass_char ; echo '')
+web_client_password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c $num_pass_char ; echo '')
 
 # Replace the string with generated passwords.
 cat docker-compose-template.yml | sed "s/___REDMINE_DB_PASSWORD___/$redmine_db_password/g" | \
 sed "s/___REDMINE_SECRET_KEY___/$redmine_secret_key/g" |
+sed "s/___WEB_CLIENT_PASSWORD___/$web_client_password/g" |
 sed "s/___MYSQL_ROOT_PASSWORD___/$mysql_root_password/g" > docker-compose.yml
